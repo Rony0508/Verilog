89ae6e9f0c04cdc44c6327db5b1f8bef697ea5de
jkjking987 <jkjking987@gmail.com>
2021-06-26 21:24:48 +0800

    626

---
+++ generator/c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    // your code goes here
+    int i,j;
+    for(i=1;i<5;i++)
+    {
+        for(j=1;j<9;j++)
+        {
+            printf("input_pref_in0%d%d,weight_pref0%d%d,",i,j,i,j);
+        }
+    }
+    return 0;
+}
